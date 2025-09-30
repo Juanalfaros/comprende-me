69ecf186d5a4b886248aa398c9bb264d56e09cd6
Juanalfaros <juan.alfaro.s@live.cl>
2025-09-30 14:09:37 -0300

    fix: update data sources

--- src/data/authorData.js
+++ src/data/authorData.js
@@ -1,6 +1,5 @@
-// src/data/authorData.js
 export const authorData = {
   name: "Natalia Beiza Calvo",
   bio: "Psicóloga clínica con más de 12 años de experiencia, dedicada a crear un espacio terapéutico seguro y humano para el bienestar de niños, adolescentes y sus familias.",
-  image: "/img/natalia/natalia.png" // Asegúrate de que esta imagen exista en public/img/natalia/
+  image: "/images/natalia/natalia-sq.webp"
 };
