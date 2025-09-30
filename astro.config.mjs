54a0ac14da06ed15ba995414311cd387d68f8be8
Juanalfaros <juan.alfaro.s@live.cl>
2025-09-30 13:37:27 -0300

    feat: Add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.comprende.me',
   output: 'server',
   adapter: netlify(),
+  integrations: [sitemap()],
 });
