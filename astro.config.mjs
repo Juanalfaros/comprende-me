76757f6748d2800341c3e16f2f5a0648d4cfbf47
Juanalfaros <juan.alfaro.s@live.cl>
2025-10-09 18:53:23 -0300

    fix: update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,19 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.comprende.me',
   output: 'server',
-  adapter: netlify(),
+  
+  // --- CORRECCIÓN AQUÍ ---
+  // Añadimos imageCDN: false para desactivar la optimización de imágenes de Netlify
+  // y forzar el uso de las imágenes optimizadas que genera Astro.
+  adapter: netlify({
+    imageCDN: false
+  }),
+
   integrations: [sitemap()],
 });
